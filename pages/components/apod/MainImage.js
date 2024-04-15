3f17dd29cf93d521a610bc282635dba6fe899515
Rashmi Momin <khanrrashmi@gmail.com>
2024-04-15 18:24:14 +1000

    Made Video Player responsive

--- pages/components/apod/MainImage.js
+++ pages/components/apod/MainImage.js
@@ -1,27 +1,26 @@
-
 import ReactPlayer from "react-player";
 
 export default function MainImage({ image_data }) {
-  const isVideo = image_data?.media_type === "video"
+  const isVideo = image_data?.media_type === "video";
 
   return (
     <div className=" flex flex-column h-auto max-w-full text-white ">
-      {
-        isVideo ? renderVideo() : renderImage()
-      }
+      {isVideo ? renderVideo() : renderImage()}
     </div>
   );
 
   function renderVideo() {
-    return <ReactPlayer
-      url={image_data?.url}
-      width="100%"
-      height="400px"
-      controls={true}
-      key={image_data?.url} />;
+    return (
+      <ReactPlayer
+        url={image_data?.url}
+        width="100%"
+        height="auto"
+        controls={true}
+        key={image_data?.url}
+      />
+    );
   }
   function renderImage() {
     return <img src={image_data?.url} />;
   }
-
 }
